9223dfd56dbc994af76fa134ae166b95f5c24a06
Avadhootdhole <162588533+Avadhootdhole@users.noreply.github.com>
2024-10-08 19:05:49 -0400

    Update robotcentric.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotcentric.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotcentric.java
@@ -2,10 +2,9 @@ package org.firstinspires.ftc.teamcode;
 
 import com.arcrobotics.ftclib.gamepad.GamepadEx;
 import com.arcrobotics.ftclib.hardware.motors.Motor;
-import com.arcrobotics.ftclib.drivebase.MecanumDrive;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.hardware.bosch.BNO055IMU;
 
 @TeleOp(name = "robotcentric", group = "Linear OpMode")
 public class robotcentric extends LinearOpMode {
@@ -15,6 +14,7 @@ public class robotcentric extends LinearOpMode {
     private Motor frontRight;
     private Motor backLeft;
     private Motor backRight;
+    private BNO055IMU imu;
     @Override
     public void runOpMode() {
         try{
@@ -23,6 +23,12 @@ public class robotcentric extends LinearOpMode {
             frontRight = new Motor(hardwareMap, "FR");
             backLeft  = new Motor(hardwareMap, "BL");
             backRight = new Motor(hardwareMap, "BR");
+            frontRight.setInverted(true);
+            backRight.setInverted(true);
+            imu = hardwareMap.get(BNO055IMU.class, "imu");
+            BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+            parameters.angleUnit = BNO055IMU.AngleUnit.RADIANS;
+            imu.initialize(parameters);
         }catch (Error e){
             telemetry.addData("Error: ", e.getMessage());
             telemetry.update();
@@ -32,18 +38,19 @@ public class robotcentric extends LinearOpMode {
             double y = -gamepad.getLeftY();  // Forward/backward input
             double x = gamepad.getRightX();   // strafing
             double rotation = gamepad.getLeftX();  // turning
-            double frontRightPower = (y + x + rotation) *-1;
-            double frontLeftPower = (y - x - rotation);
-            double backLeftPower = (y - x + rotation);
-            double backRightPower = (y + x - rotation) *-1;
+            double heading = imu.getAngularOrientation().firstAngle;
+            double rotatedStrafe = (x * Math.cos(-heading)) - (y * Math.sin(-heading));
+            double rotatedVerticalMovement = (x * Math.sin(-heading)) + (y * Math.cos(-heading));
+            double maintainRatio = Math.max(Math.abs(rotatedVerticalMovement) + Math.abs(rotatedStrafe) + Math.abs(rotation), 1);
+
             frontLeft.setZeroPowerBehavior(Motor.ZeroPowerBehavior.BRAKE);
             frontRight.setZeroPowerBehavior(Motor.ZeroPowerBehavior.BRAKE);
             backLeft.setZeroPowerBehavior(Motor.ZeroPowerBehavior.BRAKE);
             backRight.setZeroPowerBehavior(Motor.ZeroPowerBehavior.BRAKE);
-            frontLeft.set(frontLeftPower);
-            frontRight.set(frontRightPower);
-            backLeft.set(backLeftPower);
-            backRight.set(backRightPower);
+            frontLeft.set((rotatedVerticalMovement + rotatedStrafe + rotation) / maintainRatio);
+            frontRight.set((rotatedVerticalMovement - rotatedStrafe - rotation) / maintainRatio);
+            backLeft.set((rotatedVerticalMovement - rotatedStrafe + rotation) / maintainRatio);
+            backRight.set((rotatedVerticalMovement + rotatedStrafe - rotation) / maintainRatio);
 
 
 
